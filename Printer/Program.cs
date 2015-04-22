5d7bdc8c61926b24297d4051aff1b9bbe696f525
AndyYou <andyyu0920@gmail.com>
2015-04-22 17:12:01 +0800

    Add several way

--- Printer/Program.cs
+++ Printer/Program.cs
@@ -7,6 +7,8 @@ using RawDataPrint;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows.Forms;
+using PdfSharp;
+using PdfSharp.Pdf.Printing;
 
 
 namespace PrinterConsole
@@ -17,7 +19,7 @@ namespace PrinterConsole
         {
             Console.WriteLine("** START PRINT");
             /**************
-             * 範例 1 MSDN RawPrinterHelper (Failure)
+             * 範例 1 MSDN RawPrinterHelper  (EASY WAY IF WORK)
              **************/
             foreach (var printername in PrinterSettings.InstalledPrinters)
             {
@@ -36,8 +38,9 @@ namespace PrinterConsole
             }
             */
 
-            /* 發送檔案 */
+            /* 範例 1 重點使用 RawPrinterHelper 發送檔案 */
             // RawPrinterHelper.SendFileToPrinter(new PrinterSettings().PrinterName, "adobe.pdf");
+
 
             /**************
              * 範例 2 使用 Adobe Reader (Failure)
@@ -58,17 +61,70 @@ namespace PrinterConsole
             System.Threading.Thread.Sleep(3000);
             if (false == process.CloseMainWindow())
                 process.Kill();
-           */
+            */
 
             /**************
              * 範例 3 使用 Ghostscript (Failure)
              * 需安裝 Ghostscript http://www.ghostscript.com/doc/9.16/Readme.htm
              **************/
-            // var gh = new GhostHelper();
-            // gh.PrintPDF(@"C:\Program Files\gs\gs9.16\bin\gswin64c.exe", 1, "EPSON M200 Series", "adobe.pdf");
+            /*
+            ProcessStartInfo psInfo = new ProcessStartInfo();
+            psInfo.Arguments = String.Format(" -dPrinted -dBATCH -dNOPAUSE -dNOSAFER -q -dNumCopies=1 -sDEVICE=ljet4 -sOutputFile=\"\\\\spool\\{0}\" \"{1}\"", "EPSON M200 Series", "adobe.pdf");
+            psInfo.FileName = @"C:\Program Files (x86)\gs\gs9.16\bin\gswin32c.exe";
+            psInfo.UseShellExecute = false;
+            Process process = Process.Start(psInfo);
+            */
+
+            /* Using helper */
+            /*
+            var gh = new GhostHelper();
+            gh.PrintPDF(@"C:\Program Files\gs\gs9.16\bin\gswin64c.exe", 1, "EPSON M200 Series", "adobe.pdf");
+            */
+
+            /**************
+             * 範例 4 使用 PrintDocument
+             * https://msdn.microsoft.com/en-us/library/system.drawing.printing.printdocument%28v=vs.100%29.aspx
+             **************/
+            /*
+            PrintDocument document = new PrintDocument();
+            document.PrintPage += document_PrintPage;
+            // Configuration by Dialog
+            // PrintPreviewDialog ppd = new PrintPreviewDialog();
+            // ppd.Document = document;
+            // ppd.ShowDialog();  
+
+            // Configuration automatic
+            document.DocumentName = "test";
+            document.PrinterSettings.PrinterName = "EPSON M200 Series";
+            document.PrinterSettings.PrintFileName = "adobe.pdf";
+            document.PrintController = new StandardPrintController();
+            document.OriginAtMargins = false;
+            
+            document.Print();
+            */
+
+            /**************
+             * 範例 5 使用 PDFSharp (EASY WAY)
+             **************/
+            
+            PdfFilePrinter.AdobeReaderPath = @"C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe";
+            PdfFilePrinter printer = new PdfFilePrinter("adobe.pdf", "EPSON M200 Series");
+            printer.Print(5000); // After milliseconds close.
+            
+
 
             Console.WriteLine("** DONE PRINT");
             Console.ReadKey();
+
+            /**************
+             * 其他
+             **************/
+            // Working with USB Device http://www.developerfusion.com/article/84338/making-usb-c-friendly/
+        }
+
+        private static void document_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString("Hello World", new Font("Arial", 10), new SolidBrush(Color.Black), new PointF(10, 10));  
         }
 
         class GhostHelper
